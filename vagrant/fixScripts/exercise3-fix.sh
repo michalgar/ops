ac5049696895cb22eb82dddb986390eae2e27ead
michalgar <michal.garsh@gmail.com>
2019-09-02 23:00:49 +0300

    changed 1-3, added 4

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
-sudo sed -i "0,/deny from all/s//Allow from all/" /etc/apache2/sites-enabled/000-default
-sudo service apache2 restart
+sed -i "0,/deny from all/s//Allow from all/" /etc/apache2/sites-enabled/000-default
+service apache2 restart
--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -t rsa -N "" -f .ssh/id_rsa
+sed -i "s|#   StrictHostKeyChecking ask|    StrictHostKeyChecking no|g" /etc/ssh/ssh_config
+apt-get install -y sshpass && sshpass -p 'vagrant' ssh-copy-id server2
